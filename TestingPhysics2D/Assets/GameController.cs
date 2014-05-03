8bb6e333679bb12eaeb436d513b221761861e728
VictorSoudien <sdnvic001@myuct.ac.za>
2014-05-03 21:24:00 +0200

    Initial Commit

---
+++ TestingPhysics2D/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	
+	public Vector2 buttonPosition;
+
+	private Vector3 initBallPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		initBallPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	// Add a temp GUI to te game
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (buttonPosition.x, buttonPosition.y, 80, 20), "Start")) 
+		{
+			//Application.LoadLevel(1);
+			this.transform.position = initBallPosition;
+			this.transform.rotation = Quaternion.identity;
+			this.rigidbody2D.angularVelocity = 0;
+			this.rigidbody2D.velocity = Vector2.zero;
+			this.rigidbody2D.isKinematic = true;
+		}
+	}
+}

03a594a2429f6ea9297b7366b73a700608e98c4f
VictorSoudien <sdnvic001@myuct.ac.za>
2014-05-03 22:26:19 +0200

    Reset of ball position complete, temp assets used

--- TestingPhysics2D/Assets/GameController.cs
+++ TestingPhysics2D/Assets/GameController.cs
@@ -22,7 +22,7 @@ public class GameController : MonoBehaviour {
 	// Add a temp GUI to te game
 	void OnGUI()
 	{
-		if (GUI.Button (new Rect (buttonPosition.x, buttonPosition.y, 80, 20), "Start")) 
+		/*if (GUI.Button (new Rect (buttonPosition.x, buttonPosition.y, 80, 20), "Start")) 
 		{
 			//Application.LoadLevel(1);
 			this.transform.position = initBallPosition;
@@ -30,6 +30,6 @@ public class GameController : MonoBehaviour {
 			this.rigidbody2D.angularVelocity = 0;
 			this.rigidbody2D.velocity = Vector2.zero;
 			this.rigidbody2D.isKinematic = true;
-		}
+		}*/
 	}
 }

97aee59d970e97975a0d32c579ddcd61b1b196dc
VictorSoudien <sdnvic001@myuct.ac.za>
2014-05-06 22:07:44 +0200

    Screen Boundaries, Multi-screen level
    Ball now bounces off top and bottom of display. Multi-screen levels will
    now work but need to work on allowing user to view complete level.

---
+++ TestingPhysics2D/Assets/Scripts/FollowBall.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBall : MonoBehaviour {
+
+	public GameObject ball;
+	
+	private Vector3 cameraIntervalPositions;
+	private int counter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cameraIntervalPositions = this.transform.position;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (ball != null)
+		{
+			if (ball.renderer.isVisible == false)
+			{
+		 		Vector3 newCameraPosition = this.transform.position;
+		 		
+		 		if (counter > 2)
+		 		{
+		 			newCameraPosition.x = newCameraPosition.x + 15;
+		 		}
+		 		cameraIntervalPositions = newCameraPosition;
+				this.transform.position = cameraIntervalPositions;
+		 		
+		 		counter++;
+		 	}
+		 	else
+		 	{
+		 		this.transform.position = cameraIntervalPositions;
+		 	}
+		 	
+		}
+	}
+}

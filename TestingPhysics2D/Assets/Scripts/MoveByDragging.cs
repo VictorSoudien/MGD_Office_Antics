d11612c41c895ad6aaad5b2400a1894e09008148
VictorSoudien <sdnvic001@myuct.ac.za>
2014-05-15 17:44:25 +0200

    In the process of adding rotation feature
    Moving items is no longer working

--- TestingPhysics2D/Assets/Scripts/MoveByDragging.cs
+++ TestingPhysics2D/Assets/Scripts/MoveByDragging.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class MoveByDragging : MonoBehaviour {
 
+	public bool isMovable;
+
 	private Animator animator;
 	private bool selected;
-
-	public bool isMovable;
+	private GameObject selectionCircle;
 
 	// Use this for initialization
 	void Start () 
@@ -14,6 +15,8 @@ public class MoveByDragging : MonoBehaviour {
 		animator = this.GetComponent<Animator> ();
 		selected = false;
 		isMovable = true;
+
+		selectionCircle = GameObject.Find ("SelectionCircle");
 	}
 
 	// Update is called once per frame
@@ -23,32 +26,66 @@ public class MoveByDragging : MonoBehaviour {
 		{
 			if (Input.GetButton ("Fire1") == true) 
 			{
-					Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-					Vector2 touch2D = new Vector2 (target.x, target.y);
+				Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				Vector2 touch2D = new Vector2 (target.x, target.y);
 
-					if (selected == false) 
+				/*if (selected == false) 
+				{
+					if (this.collider2D == Physics2D.OverlapPoint (touch2D)) 
 					{
-						if (this.collider2D == Physics2D.OverlapPoint (touch2D)) 
-						{
-							animator.SetBool ("Touched", true);
-							selected = true;
-							this.transform.position = new Vector3 (target.x, target.y + 0.5f, 0);
-						}
-					} 
-					else 
-					{
+						animator.SetBool ("Touched", true);
+						selected = true;
 						this.transform.position = new Vector3 (target.x, target.y + 0.5f, 0);
 					}
-			} 
-			else 
+				} 
+				else 
+				{
+					this.transform.position = new Vector3 (target.x, target.y + 0.5f, 0);
+
+					// Draw the selection circle for the item
+					if (selectionCircle != null)
+					{
+						selectionCircle.renderer.enabled = true;
+					}
+				}*/
+
+				if (this.collider2D == Physics2D.OverlapPoint (touch2D)) 
+				{
+					selected = true;
+				}
+				else
+				{
+					selected = false;
+				}
+			}
+			/*else 
 			{
 				selected = false;
 				animator.SetBool ("Touched", false);
-			}
+
+				// Draw the selection circle for the item
+				if (selectionCircle != null)
+				{
+					selectionCircle.renderer.enabled = false;
+				}
+			}*/
 		}
 		else
 		{
 			// Play sound here and vibrate phone
+		}
+
+		if (selectionCircle != null) 
+		{
+			if (selected == true)
+			{
+				selectionCircle.transform.position = this.transform.position;
+				selectionCircle.renderer.enabled = true;
+			}
+			/*else
+			{
+				selectionCircle.renderer.enabled = false;
+			}*/
 		}
 	}
 }

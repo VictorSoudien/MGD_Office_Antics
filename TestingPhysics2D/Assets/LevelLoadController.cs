8e707af5df937f8a3208f9c27bff0fd8a5cea715
VictorSoudien <sdnvic001@myuct.ac.za>
2014-05-26 22:09:23 +0200

    Level selection mostly complete
    - Still need to put bounds on where camera can move

--- TestingPhysics2D/Assets/LevelLoadController.cs
+++ TestingPhysics2D/Assets/LevelLoadController.cs
@@ -1,24 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 public class LevelLoadController : MonoBehaviour {
 
 	public int levelIndex;
 	
+	public List<GameObject> listOfLevelSelectors;
+	
 	private bool swiped;
 	private Vector3 targetPosition;
+	
+	float startTime;
 
 	// Use this for initialization
 	void Start () 
 	{
 		swiped = false;
+		Vector3 newPosition = new Vector3(-0.2948666f, -0.07090187f, 0);
+		
+		foreach (GameObject gameObj in listOfLevelSelectors)
+		{
+			gameObj.transform.position = newPosition;
+			newPosition.x += 11.285f;
+		}
+	}
+	
+	// Dsiplays the next level cover
+	private void displayNext()
+	{
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if ((Input.touchCount == 1) && (Input.GetTouch(0).phase == TouchPhase.Moved))
+		if ((Input.touchCount == 1) && (Input.GetTouch(0).phase == TouchPhase.Began))
 		{
+			startTime = Time.deltaTime;
+		}
+		else if ((Input.touchCount == 1) && (Input.GetTouch(0).phase == TouchPhase.Ended))
+		{
+			startTime += Time.deltaTime;
+			
+			if (startTime < 0.05)
+			{
+				Vector2 touchLocation = new Vector2 (Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+				Vector2 origin = new Vector2 (this.transform.position.x, this.transform.position.y);
+				
+				RaycastHit2D hit = Physics2D.Raycast(this.transform.position, touchLocation);
+				
+				if (hit != null)
+				{
+					int levelIndex = listOfLevelSelectors.IndexOf(hit.collider.gameObject);
+					
+					if (levelIndex != -1)
+					{
+						Application.LoadLevel(levelIndex + 2);
+					}
+				}
+			}
+		}
+		else if ((Input.touchCount == 1) && (Input.GetTouch(0).phase == TouchPhase.Moved))
+		{
+			startTime += Time.deltaTime;
 			if (swiped == false)
 			{
 				swiped = true;
@@ -30,22 +76,12 @@ public class LevelLoadController : MonoBehaviour {
 			
 			if (this.transform.position != targetPosition)
 			{
-				this.transform.Translate(deltaTouch.x * 0.1f, 0, 0f);
+				this.transform.Translate(-deltaTouch.x * 0.1f, 0, 0f);
 			}
 		}
 		else if (Input.touchCount == 0)
 		{
 			swiped = false;
-		}
-		else if (Input.touchCount == 1)
-		{
-			Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			Vector2 touch2D = new Vector2 (target.x, target.y);
-			
-			if (this.collider2D.OverlapPoint(touch2D))
-			{
-				Application.LoadLevel(levelIndex);
-			}
 		}
 	}
 }
